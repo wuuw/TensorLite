53c0a55c33128c6a37cc749cbd11494d33a13bb4
wuuw <w876629399u@qq.com>
2019-07-24 16:35:20 +0800

    computational graph instance comment

--- tensorlite/operations.py
+++ tensorlite/operations.py
@@ -32,19 +32,40 @@ class matmul(Operation):
     def compute(self, x_value, y_value):
         return np.dot(x_value, y_value)
 
+"""
+通过一下代码，构建了一个简单的计算图：
+y = A * x
+z = y + b
+
+A:Variable    ->|
+                |------> y ------>|
+x:placeholder ->|                 |---> z
+                    b:Variable -->|
+                    
+A.consumers = [y]
+x.consumers = [y]
+
+y.input_nodes = [A, x]
+y.consumers = [z]
+
+b.consumers = [z]
+
+z.input_nodes = [y, b]
+z.consumers = []
+"""
 
 # 创建计算图
-Graph().as_default()
+# Graph().as_default()
 
 # variables
-A = Variable([[1, 0], [0, -1]])
-b = Variable([1, 1])
+# A = Variable([[1, 0], [0, -1]])
+# b = Variable([1, 1])
 
 # placeholder
-x = placeholder()
+# x = placeholder()
 
 # 中间节点 y
-y = matmul(A, x)
+# y = matmul(A, x)
 
 # Operation 的输出节点 z
-z = add(y, b)
+# z = add(y, b)
